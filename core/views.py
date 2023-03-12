40b1e5c03a6172dd24cca4181327374543eb8917
unknown <aslam6888@gmail.com>
2023-03-12 20:59:30 +0530

    drf added

---
+++ core/views.py
@@ -0,0 +1,19 @@
+from django.shortcuts import render
+from rest_framework.response import Response
+from rest_framework.decorators import api_view
+from .models import Products
+# Create your views here.
+
+@api_view(['GET'])
+def ProductList(request):
+    """
+        Products List
+
+    Args:
+        request (_type_): _description_
+
+    Returns:
+        _type_: _description_
+    """
+    data = Products.objects.all()
+    return Response(data.values())
